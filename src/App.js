19dd7377b4fc196719f88593267695c96c98bc12
Chandan Kumar <42267102+chandanrajput2@users.noreply.github.com>
2021-08-22 12:15:10 +0530

    my first commit app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+
+import Registration from './components/Registration/Registration';
+function App() {
+  return (
+    <div className="App">
+      <div className="outer">
+        <div className="inner">
+        {/* <h1>helllo</h1> */}
+          <Registration />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
